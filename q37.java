8bb6646f9cc8375ca90047faf8a71f6d36c9bffa
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-08-31 12:26:54 -0400

    Add files via upload
    /*reverse a string 
     * This utilizes a char array to allow easy loop access
     * then simply reverse the array to print out
     * brute force approach
    */

---
+++ q37.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class q37 {
+    public static void main(String[] args){
+        String word = "Hello";
+       // char []  letters = word.toString();
+       Scanner scanner=new Scanner (System.in);
+       System.out.println("enter a string: ");
+       char [] letters = scanner.nextLine().toCharArray();
+       System.out.println("reversed WORD string: ");
+       for(int i =letters.length-1;i >= 0;i--){
+        System.out.print(letters[i]);
+       }
+
+
+
+    }
+    
+}
+/*reverse a string */
