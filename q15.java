b6b8e0a7dd290f5a6386bbc35feada22c03eedcd
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-08-30 21:48:48 -0400

    Add files via upload
    This java program swaps numbers

---
+++ q15.java
@@ -0,0 +1,14 @@
+public class q15 {
+    public static void main(String[] args){
+        int num1 = 2;
+        int num2 = 3;
+        System.out.println("before swap "+ num1+","+num2);
+        int temp = 1;
+        //
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("before AFTER "+ num1+","+num2);
+    }
+}
