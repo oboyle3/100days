e4ea413c8c622b8f4ab63fbf141dd6a0713d6e14
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-01 17:01:47 -0400

    Add files via upload

---
+++ q44.java
@@ -0,0 +1,11 @@
+public class q44 {
+    public static void main(String[] args){
+    int n = 5;
+    System.out.println("input number: "+ n);
+    //System.out.print("%d + %d%d + %d%d%d" ,input,input,input,input,input,input);
+    System.out.printf("%d + %d%d + %d%d%d", n, n, n, n, n, n);
+
+    }
+    
+}
+//Write a Java program that accepts an integer (n) and computes the value of n+nn+nnn
