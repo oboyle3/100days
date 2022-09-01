f7840c7f5b3948cf6fd6377606624eee5593648b
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-01 17:16:10 -0400

    Add files via upload

---
+++ q49.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class q49 {
+        public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = scanner.nextInt();
+
+        if(n%2 == 0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+
+
+
+        }
+}
+//Write a Java program to accept a number and check the number is even or not. Prints 1 if the number is even or 0 if the number is odd
