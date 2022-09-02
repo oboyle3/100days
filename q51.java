c05e8c8299f88befb36fbfe23331c2ee35926472
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-02 11:47:49 -0400

    Add files via upload

---
+++ q51.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class q51 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number to be converted to a string");
+        String word = scanner.nextLine();
+        int num = Integer.parseInt(word);
+        System.out.println("converted to a string "+ num);
+
+
+    }
+}
+//Write a Java program to convert a string to an integer in Java
