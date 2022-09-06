3f201986030d3e2f517db6a6d5590e7f5b30b3c1
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-05 21:36:29 -0400

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,8 @@
+public class q2 {
+    public static void main(String[] args){
+        int num = 2;
+        int num2 = 3;
+        int sum = num+num2;
+        System.out.println("sum: "+ sum);
+}}
+
