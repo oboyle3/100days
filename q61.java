5f6fed1c0c235672cfb1dc7a5e10d7a1872ca653
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-06 19:19:44 -0400

    Add files via upload

---
+++ q61.java
@@ -0,0 +1,23 @@
+public class q61 {
+    public static void main(String[] args){
+
+        String word = "hello";
+
+        char [] word2 = word.toCharArray();
+
+        for ( int i = word2.length -1 ; i >= 0 ; i-- ){
+            System.out.print(word2[i]);
+        }
+
+
+
+
+
+
+
+
+    }
+        
+    
+}
+//Write a Java program to reverse a word
