6a318a55b4147d9cf3a1921cff8cad1a18076b3b
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-06 10:19:57 -0400

    Add files via upload

---
+++ q52.java
@@ -0,0 +1,24 @@
+public class q52 {
+    public static void main(String[] args){
+
+        int num1 = 8;
+
+        int num2 = 10;
+        
+        int num3 = 10;
+
+        if(num1 + num2 == num3 ){
+            System.out.println("this is true!");
+        }
+        else{
+            System.out.println("False False!");
+        }
+
+
+    }
+    
+}
+
+/*
+ * Write a Java program to calculate the sum of two integers and return true if the sum is equal to a third integer
+ */
