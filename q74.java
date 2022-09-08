51c4b8b0280e6813d8d9e19dea7ac7bff1ad9eab
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-08 12:50:09 -0400

    Add files via upload

---
+++ q74.java
@@ -0,0 +1,18 @@
+public class q74 {
+    public static void main(String[] args){
+
+        int arr []={10,12,39,2,12};
+        
+        int test = 10;
+
+        if(test == arr[0]){
+            System.out.println("10 is the first elemnty");
+        }
+        if(test == arr[arr.length-1]){
+            System.out.println("10 is the last elemnty");
+        }
+
+
+    }
+    
+}
