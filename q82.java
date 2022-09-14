2f0b14b7a670ebbe5b4ce010eae165f5806909cc
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-13 20:34:23 -0400

    Add files via upload

---
+++ q82.java
@@ -0,0 +1,16 @@
+public class q82 {
+    public static void main(String[] args){
+        int [] arr = {20, 30, 40, 50, 67};
+
+        int small = arr[0];
+        int middle=arr[2];
+        int large = arr[arr.length-1];
+
+
+    }
+    
+}
+/*82. Write a Java program to find the largest element between first, last, and middle values from an array of integers (even length). 
+Sample Output:
+Original Array: [20, 30, 40, 50, 67]                                   
+Largest element between first, last, and middle values: 67  */
