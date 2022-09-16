f16150f2ac9dbba345173115c384c01dac3f7a80
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-16 18:11:40 -0400

    Add files via upload

---
+++ q85.java
@@ -0,0 +1,11 @@
+public class q85 {
+    public static void main(String[] args){
+        String check = "hello pat";
+        System.out.println(check.startsWith("hello"));
+
+    }
+    
+}
+/*85. Write a Java program to check if a string starts with a specified word. Go to the editor
+Sample Data: string1 = "Hello how are you?"
+Sample Output: */
