3ce6d0c668cd9f904384cda55719eaeac464ee64
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-09-20 21:04:12 -0400

    Add files via upload

---
+++ q92.java
@@ -0,0 +1,25 @@
+public class q92{
+   public static void main(String[] args){
+   
+   int [] arr = {1/*,2,3,4,5*/};
+   
+   int countEven = 0;
+   int countOdd = 0;
+   
+   for(int i =0; i <arr.length;i++){
+      if(arr[i] % 2 == 0){
+      System.out.println("even num");
+      }
+   
+   
+   }
+   
+   
+   
+   
+   
+   }//end main
+
+
+}
+/* Write a Java program to count the number of even and odd elements in a given array of integers*/
