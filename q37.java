348b64821d81a37d4dbfd18f0058e26450e00405
oboyle3 <55725426+oboyle3@users.noreply.github.com>
2022-10-04 22:06:12 -0400

    Update q37.java

--- q37.java
+++ q37.java
@@ -6,7 +6,7 @@ public class q37 {
        Scanner scanner=new Scanner (System.in);
        System.out.println("enter a string: ");
        char [] letters = scanner.nextLine().toCharArray();
-       System.out.println("reversed WORD string: ");
+       System.out.println("reversed Word string: ");
        for(int i =letters.length-1;i >= 0;i--){
         System.out.print(letters[i]);
        }
